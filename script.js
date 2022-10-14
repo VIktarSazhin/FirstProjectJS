b5644b1c894d5e95f07c16f15830e20ea4615c71
v.sazhyn <v.sazhyn@andersenlab.com>
2022-10-14 12:15:42 +0300

    firs task

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const numberOfFilms = +prompt("How many films are you watching?", "");
+const response1 = "";
+const numberOfFilms = +prompt("How many films are you watching?", response1);
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,13 +11,27 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt("What is the last fils are you watching?", ""),
-      b = prompt("What is his rating?", ""),
-      c = prompt("What is the last fils are you watching?", ""),
-      d = prompt("What is his rating?", "");
 
+for(let i = 0; i < 2; i++) {
+    const a = prompt("What is the last fils are you watching?", ""),
+          b = prompt("What is his rating?", "");
+    
+    if(a != null && b != null &&  a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a]= b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[a]= b;
-personalMovieDB.movies[c]= d;
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else {
+    alert("Произошла ошибка");
+}
+
 
 console.log(personalMovieDB);
